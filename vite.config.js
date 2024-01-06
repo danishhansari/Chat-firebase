20577e7ccded16a536b32fd70856ef80ac10c271
Danish <dan71ish@gmail.com>
2024-01-06 12:14:50 +0530

    remove million because it is making slower to load firebase

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import million from "million/compiler";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    million.vite({
-      auto: {
-        threshold: 0.05,
-        skip: ["useBadHook", /badVariable/g],
-      },
-    }),
-    react(),
-  ],
+  plugins: [react()],
 });
